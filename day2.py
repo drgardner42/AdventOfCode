9069fdd4003c7e669300c4923c29d0f4291d01c1
Dustin <Drgardner42@students.tntech.edu>
2015-12-07 18:55:57 -0500

    part1 complete

--- day2.py
+++ day2.py
@@ -6,32 +6,22 @@ sum = 0
 for i in lines:
 	i = i.strip('\n')
 	dimensions = i.split('x')
-	print (dimensions)
 	l = int(dimensions[0])
 	w = int(dimensions[1])
 	h = int(dimensions[2])
-	wrappingPaper = 2*l*w + 2*w*h + 2*h*l
-	if l < w and l < h:   #if length is least
-	   if w < h:
-	      slack = l*w
-	      print ("l*w")
-	   else:
-	      slack = l*h
-	      print ("l*h")
-	elif w < l and w < h: #if width is least
-	   if l < h:
-	      slack = w*l
-	      print ("w*l")
-	   else:
-	      slack = w*h
-	      print ("w*h")
-	else:                 #height is least
-	   if l < w:
-	      slack = h*l
-	      print ("h*l")
-	   else:
-	      slack = h*w
-	      print ("h*w")
+	
+	d1 = l*w
+	d2 = w*h
+	d3 = h*l
+	wrappingPaper = (2*d1) + (2*d2) + (2*d3)
+	slack = 0
+
+	if d1 <= d2 and d1 <= d3:
+	   slack = d1
+	elif d2 <= d1 and d2 <= d3:
+	   slack = d2
+	else:
+	   slack = d3
+
 	sum = sum + slack + wrappingPaper
-	print(sum)
 print (sum)
