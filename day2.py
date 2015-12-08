539dc723d91dd3ab194e89b6fbe22c4c6b750119
Dustin <Drgardner42@students.tntech.edu>
2015-12-08 10:58:45 -0500

    complete

--- day2.py
+++ day2.py
@@ -3,6 +3,7 @@ lines = f.readlines()
 f.close()
 
 sum = 0
+ribbonSum = 0
 for i in lines:
 	i = i.strip('\n')
 	dimensions = i.split('x')
@@ -10,11 +11,24 @@ for i in lines:
 	w = int(dimensions[1])
 	h = int(dimensions[2])
 	
+	bow = l*w*h
+	p1 = l+l+w+w
+	p2 = l+l+h+h
+	p3 = w+w+h+h
+	
 	d1 = l*w
 	d2 = w*h
 	d3 = h*l
 	wrappingPaper = (2*d1) + (2*d2) + (2*d3)
 	slack = 0
+	ribbon = 0
+
+	if p1 <= p2 and p1 <= p3:
+	   ribbon = p1
+	elif p2 <= p1 and p2 <= p3:
+	   ribbon = p2
+	else:
+	   ribbon = p3
 
 	if d1 <= d2 and d1 <= d3:
 	   slack = d1
@@ -24,4 +38,6 @@ for i in lines:
 	   slack = d3
 
 	sum = sum + slack + wrappingPaper
+	ribbonSum += ribbon + bow
 print (sum)
+print (ribbonSum)
